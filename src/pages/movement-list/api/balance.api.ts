e7a8264ad203bae4d36e6be45b6ba17da07d17cb
migalko94 <miguelgalvezugr@gmail.com>
2023-09-25 11:43:20 +0200

    fixing merge

--- src/pages/movement-list/api/balance.api.ts
+++
@@ -1,8 +0,0 @@
-import Axios from "axios";
-
-import { Account } from "@/pages/account-list/api";
-
-const urlIban = `${import.meta.env.VITE_BASE_API_URL}/account-list/`;
-
-export const getBalance = (accountId: string): Promise<string | number> =>
-  Axios.get<Account>(`${urlIban}${accountId}`).then(({ data }) => data.balance);
--- src/pages/movement-list/api/index.ts
+++ src/pages/movement-list/api/index.ts
@@ -1,3 +1,2 @@
 export * from "./movement-list.api";
 export * from "./movement-list.api-model";
-export * from "./movement-list.api";
